a8bb195928c41429cdbfda625ea4778105d3dad1
Franz <franzhflores@hotmail.com>
2018-04-06 15:06:33 -0400

    Kick off the solution student app, added the testting project

---
+++ StudentSolution/StudentSolution.UnitTest/CreateNewStudent_Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using StudentSolution.Business;
+using StudentSolution.Data;
+namespace StudentSolution.UnitTest
+{
+    [TestClass]
+    public class CreateNewStudent_Test
+    {
+        [TestMethod]
+        public void CreateNewStudent_CheckByName()
+        {
+            
+            IStudentManager manager = new StudentManager();
+            int iNumberOfStudent = students.count;
+            Student szJhon = new Student()
+            {
+                Type: "High",
+                Name: "Jhon",
+                Gender: "Male",
+                TimeSpam: DateTime
+
+            };
+            students.Add(szJhon);
+            string szResult = manager.Exists(szJhon.Name);
+            Assert.AreEqual(iNumberOfStudent, szResult);
+    }
+}
---
+++ StudentSolution/StudentSolution.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StudentSolution.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StudentSolution.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14f7d597-7915-4e69-a6bd-268428278896")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
