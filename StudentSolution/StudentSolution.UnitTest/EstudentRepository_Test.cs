22b6ab73cdd8c34e6c38d6744de4173bf00c0838
Franz <franzhflores@hotmail.com>
2018-04-07 09:22:48 -0400

    Write unit test for add  item to student repository

---
+++ StudentSolution/StudentSolution.UnitTest/EstudentRepository_Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudentSolution.UnitTest
+{
+    [TestClass]
+    public class EstudentRepository_Test
+    {
+        [TestMethod]
+        public void Add_New_Student()
+        {
+            List<Student> oStudents = new List<Student>();
+            Student oInputStudent = helper.Createone();
+            IStudentRespository oRepo = new StudentRespository(context);
+            oRepo.Add(oInputStudent);
+
+            Student oExpectResult = oRepo.Search(oInputStudent.Name);
+
+            Assert.IsNotNull(oExpectResult);
+        }
+    }
+}
